3cf8fe5161cb05c7173d04a37ffc3ace5afe1abd
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-05 16:29:45 +0530

    without i & with i

--- Recursion/Remove_Duplicate.cpp
+++ Recursion/Remove_Duplicate.cpp
@@ -27,34 +27,4 @@ int main()
     return 0;
 
 
-} 
-//without int i
-#include<iostream>
-#include<vector>
-#include<string>
-using namespace std;
-void rd(string s,string ans,vector<bool> m){
-    if(s.empty()){
-        cout<<ans<<endl;
-        return;
-    }
-     int c= s[s.size()-1] - 'a';
-    if(!m[c])
-    {   m[c]= true;
-         rd(s.substr(0,s.size() - 1),ans + s[s.size() - 1],m);
-    }else{
-     rd(s.substr(0,s.size() - 1),ans,m);
 }
-}
-int main()
-{
-    string s = "hellllooohell";
-    vector<bool> map(26,false);
-
-    string ans;
-    
-    rd(s,ans,map);
-    return 0;
-
-
-} 
