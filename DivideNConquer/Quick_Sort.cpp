880f1bf2f8b2f76f701636d6ff47695090355262
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-05 23:23:25 +0530

    quick sort with pivot

---
+++ DivideNConquer/Quick_Sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition(int arr[],int si,int ei){
+    int pivot=arr[ei];
+    int i=si-1;
+    int j=si;
+    for (int j = si; j < ei; j++) {//shift all elements smaller than pivot to the left
+        if (arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    i++;
+swap(arr[i],arr[ei]);
+return i;    
+    }
+    void quicksort(int arr[],int si,int ei){
+        if(si>=ei){
+            return;
+
+        }
+        int pivot=partition(arr,si,ei);//chose the pivot and partition the array for every partition we get a sorted array
+        quicksort(arr,si,pivot-1);//lefft half
+        quicksort(arr,pivot+1,ei);//right half
+
+    }
+    void printarr(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }}
+    int main(){
+        int arr[]={5,2,9,1,7,3};
+        int n = sizeof(arr)/sizeof(arr[0]);
+        quicksort(arr,0,n-1);
+        printarr(arr,n);
+        return 0;
+    }
