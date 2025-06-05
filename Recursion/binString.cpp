b93b7b3cad2dfa61aab99b57cc83ca93f6c52898
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-05 18:00:55 +0530

    binary strig added

---
+++ Recursion/binString.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void bin(int n,string ans){
+    if(n==0){
+        cout<<ans<<endl;
+    return;
+}
+if(ans[ans.size() - 1] == '0'){
+bin(n-1,ans + "0");
+bin(n-1,ans+"1");
+}
+else{
+    bin(n-1,ans + "0");
+}
+
+}
+int main()
+{string ans = "";
+   bin(5,ans); 
+   return 0;
+   
+}
