e0bfd3fb4856da7212113e63fe10be1561426014
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-07 15:53:07 +0530

    Day 24

---
+++ Backtracking/Subset.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+using namespace std;
+void printsubset(string s,string sb){
+    
+    if(s.empty()){
+        cout<<sb<<endl;
+      return;
+    }
+     printsubset(s.substr(1,s.length()-1),sb);
+
+    printsubset(s.substr(1,s.length()-1),sb+s[0]);
+   
+}
+int main(){
+    string s = "xyz";
+    string sb ="";
+    printsubset(s,sb);
+}
