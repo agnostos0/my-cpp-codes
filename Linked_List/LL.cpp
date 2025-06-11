ef09f90529e7218c4120dd35a9aab82e153b7b41
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-11 13:41:23 +0530

    Day 25

--- Linked_List/LL.cpp
+++ Linked_List/LL.cpp
@@ -85,9 +85,8 @@ void pop_fornt(){
     }
     Node* temp = head;
     head = head->next;
-    temp = NULL;
     delete temp;
-    
+
 
 }
 void pop_back(){
@@ -107,6 +106,60 @@ void pop_back(){
    tail = temp;
    tail->next = NULL;
 }
+int findN(int key){
+    Node* temp = head;
+    int count = 0;
+    while (temp != NULL) {
+        if(temp->data == key){
+            
+            cout<<count<<endl;
+            return count;
+        }
+        temp = temp->next;
+        count++;
+       
+}
+ return -1;
+}
+int size(){
+    Node* temp = head;
+    int count = 0;
+    while (temp != NULL) {
+        temp = temp->next;
+        count++;
+       
+}
+return count;
+}
+void remove(int n){
+     int len = size();
+    if (n > len || n < 1) {
+        cout << "Invalid position to remove" << endl;
+        return;
+    }
+
+    // Special case: delete head
+    if (n == len) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        if (head == NULL) tail = NULL; // list became empty
+        return;
+    }
+
+        Node* temp = head;
+    // Traverse to the (len - n)th node
+    for (int i = 1; i < len - n; ++i) {
+          temp = temp->next;
+    }
+    // Remove the (len - n + 1)th node
+    Node* nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    if (nodeToDelete == tail) tail = temp;
+    nodeToDelete->next = NULL;
+    delete nodeToDelete;
+    
+}
 };
 
 int main(){
@@ -116,7 +169,7 @@ int main(){
     ll.push_front(1);
     ll.push_back(4);
     ll.push_back(5);
-   ll.pop_fornt();
-    ll.pop_back();
+    ll.remove(1);
     ll.printList();
+   
 }
