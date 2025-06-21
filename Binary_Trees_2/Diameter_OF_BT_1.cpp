7e84ff0f29c08a244692be5f7ec7a390dfb2bb3c
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-21 22:18:56 +0530

    Day 32

---
+++ Binary_Trees_2/Diameter_OF_BT_1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value) {
+        data = value;
+        left = right = NULL;
+    }
+};
+
+// Global variables (used for simplicity here)
+static int idx = -1;
+static int depth = 0;
+
+// Function to build tree from vector (preorder style)
+Node* buildtree(const vector<int>& data) {
+    idx++;
+    if (data[idx] == -1) {
+        return NULL;
+    }
+    Node* curr = new Node(data[idx]);
+    curr->left = buildtree(data);
+    curr->right = buildtree(data);
+    return curr;
+}
+
+int height(Node* root) {
+    if (root == NULL) {
+      
+        return 0 ;
+    }
+    height(root->left);
+    height(root->right);
+    int currheight = max(height(root->left), height(root->right));
+    return currheight + 1;
+ 
+}
+int dia(Node* root){//O(n^2)
+    
+    if(root==NULL){
+        return 0;
+    }
+    int currheight = height(root->left) + height(root->right) + 1;
+    int leftdia = dia(root->left);
+    int rightdia = dia(root->right);
+    return max(currheight,max(leftdia,rightdia));
+}
+
+int main() {
+    vector<int> data = {1, 2, -1, -1, 3, -1, 4, -1, -1};
+    Node* root = buildtree(data);
+    cout<<dia(root);
+
+}
