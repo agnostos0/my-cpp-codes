5ab254c38daa3f67cfb5a910a641319885522545
agnostos0 <hetmangukiya9293@gmail.com>
2025-06-27 22:55:26 +0530

    Day 33

--- Binary_Trees_3/SumTree.cpp
+++ Binary_Trees_3/SumTree.cpp
@@ -30,15 +30,55 @@ int transform(Node* root){
     int left = transform(root->left);
     int right = transform(root->right);
     int currold = root->data;
-    root->data = left + right + root->left->data + root->right->data;
+    root->data = left + right ;
+    if(root->left !=NULL){
+        root->data += root->right->data;
+    }
+     if(root->right !=NULL){
+        root->data += root->right->data;
+    }
     return currold;
    
 
 
 
 }
+void levelordertraversel(Node* root){
+    if(root==NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* currnod = q.front();
+        q.pop();
+     if(currnod==NULL){
+            cout<<endl;
+            if(q.empty()){
+                break;
+            }
+         q.push(NULL);
+        }
+        else{
+        cout<<currnod->data<<". ";
+        if(currnod->left != NULL)
+      { 
+         q.push(currnod->left);
+
+          }
+
+
+          if(currnod->right!= NULL)
+          {
+           q.push(currnod->right);
+        } }
+}
+}
 int main(){
     vector<int> data = {1,2,3,-1,-1,4,-1,-1,5,6,-1,-1,9,-1,-1};
     Node* root = buildtree(data);
+    transform(root);
+    levelordertraversel(root);
 
 }
