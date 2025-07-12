b0f0b76b1e6d3348e76adac5980b1dbbd5fa2623
agnostos0 <hetmangukiya9293@gmail.com>
2025-07-12 20:39:55 +0530

    Day 38

--- Hashing_1/HashTable.cpp
+++ Hashing_1/HashTable.cpp
@@ -15,3 +15,41 @@ class Node{
         next = NULL;
     }
 };
+int Hashing(string key){
+    int idx =0 ;
+    for(int i=0;i<key.length();i++){
+        idx = idx + (key[i]*key[i])%key.length();
+    }
+    return idx;
+}
+class HashTable{
+    public:
+    int totalsize;
+    int currsize;//for rehsashing
+    Node** table;
+    HashTable(int size){
+        currsize = 0;
+        
+        totalsize = size;
+        table = new Node*[totalsize];
+        for(int i = 0;i<totalsize;i++){
+            table[i] = NULL;
+        }
+    }
+    void insert(int value,string key){
+        int idx = Hashing(key);
+        currsize++;
+        if(table[idx] == NULL){
+            table[idx] = new Node(value,key);
+    }
+    else{
+        Node* temp = table[idx];
+        table[idx] = new Node(value,key);
+        table[idx]->next = temp;
+
+        
+    }
+
+}
+
+};
